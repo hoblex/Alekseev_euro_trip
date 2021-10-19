803cd91ec6b89a173061b2c5ef02920991ae6966
Aleksandr Alekseev <hoblex@gmail.com>
2021-10-20 01:16:17 +0300

    стилизация предложений

--- source/js/script.js
+++ source/js/script.js
@@ -1,42 +1,3 @@
-// //Обработка событий кнопки открытия/закрытия главного меню в мобильном режиме
-//
-// var navMain = document.querySelector('.header-nav');
-// var navToggle = document.querySelector('.header-nav__toggle');
-//
-// navMain.classList.remove('header-nav--nojs');
-// navMain.classList.add('header-nav--closed');
-//
-//   navToggle.addEventListener('click', function() {
-//   if (navMain.classList.contains('header-nav--closed')) {
-//   navMain.classList.remove('header-nav--closed');
-//   navMain.classList.add('header-nav--opened');
-// } else {
-//   navMain.classList.add('header-nav--closed');
-//   navMain.classList.remove('header-nav--opened');
-// }
-// });
-//
-// //Обработка событий модального окна "Заказ товара дня"
-//
-// var buttonProductToBuy = document.querySelectorAll('.form-to-order');
-// var displayModalProductToBuy = document.querySelector('.modal-cart');
-//
-// for (var i = 0; i < buttonProductToBuy.length; i++) {
-//   buttonProductToBuy[i].addEventListener('click', function (evt) {
-//     evt.preventDefault();
-//     displayModalProductToBuy.classList.add('modal-cart--show');
-//   });
-// }
-//
-// window.addEventListener('keydown', function (evt) {
-//   if (evt.keyCode === 27) {
-//     if (displayModalProductToBuy.classList.contains('modal-cart--show')) {
-//       evt.preventDefault();
-//       displayModalProductToBuy.classList.remove('modal-cart--show');
-//     }
-//   }
-// });
-
 const placeCardList = Array.from(document.querySelectorAll('.place-card__link'));
 const buttonNavList = Array.from(document.querySelectorAll('.slider-nav__button'));
 const overviewsList = Array.from(document.querySelectorAll('.overviews__item'));
@@ -61,7 +22,7 @@ buttonNavList.forEach((item) => item.addEventListener('click', (evt) => {
 const modalWindow = document.querySelector('.modal-buy');
 
 const modalWindowClose = modalWindow.querySelector('.modal-buy__close');
-const overviewButtonOpenModal = document.querySelector('.overview__order-link');
+const buttonOpenModal = Array.from(document.querySelectorAll('.order-link'));
 
 const modalInputPhone = modalWindow.querySelector('.modal-buy__form-control[type="tel"]');
 const modalInputEmail = modalWindow.querySelector('.modal-buy__form-control[type="email"]');
@@ -84,15 +45,17 @@ const keyDownHandler = function (evt) {
     return handler;
 }
 
-overviewButtonOpenModal.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    modalWindow.classList.add('modal-buy--show');
-    document.addEventListener('keydown', keyDownHandler('keydown'));
-    modalInputPhone.setCustomValidity('');
-    modalForm.reset();
-    modalLabelError[0].style.display = "none";
-    modalLabelError[1].style.display = "none";
-});
+buttonOpenModal.forEach((item) => {
+    item.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        modalWindow.classList.add('modal-buy--show');
+        document.addEventListener('keydown', keyDownHandler('keydown'));
+        modalInputPhone.setCustomValidity('');
+        modalForm.reset();
+        modalLabelError[0].style.display = "none";
+        modalLabelError[1].style.display = "none";
+    });
+})
 
 modalWindowClose.addEventListener('click', (evt) => {
     if (modalWindow.classList.contains('modal-buy--show')) {
